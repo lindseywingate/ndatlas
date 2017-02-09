ee60de6f0028845f692748d4b11e03b38e9aaba0
Lindsey Wingate <lindseymwingate@gmail.com>
2017-02-09 21:50:50 +0100

    got timed slideshows working. just need to figure out if other buttons should be available during the slideshow time

--- public/js/wheat.js
+++ public/js/wheat.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-        $("#a_1949").on("click", function() {
+		var interval;
+		$("#a_1949").on("click", function() {
                 $("#map").attr("src", "/images/maps/BWheat_1949.png");
+				clearAll(interval);
         });
         $("#a_1954").on("click", function() {
                 $("#map").attr("src", "/images/maps/BWheat_1954.png");
@@ -13,6 +15,7 @@ $(document).ready(function() {
         });
         $("#a_1969").on("click", function() {
                 $("#map").attr("src", "/images/maps/BWheat_1969.png");
+			
         });
         $("#a_1974").on("click", function() {
                 $("#map").attr("src", "/images/maps/BWheat_1974.png");
@@ -40,33 +43,40 @@ $(document).ready(function() {
         });
         $("#a_2012").on("click", function() {
                 $("#map").attr("src", "/images/maps/BWheat_2012.png");
-        });
-		
+		});
+	
 		var map_array = ["BWheat_1949.png", "BWheat_1954.png", "BWheat_1959.png", "BWheat_1964.png", "BWheat_1969.png", "BWheat_1974.png", "BWheat_1978.png", "BWheat_1982.png", "BWheat_1987.png", "BWheat_1992.png", "BWheat_1997.png", "BWheat_2002.png", "BWheat_2007.png", "BWheat_2012.png"];
-		var interval;
 		$("#play1").on("click", function() {
+			$("#play1").prop("disabled", true);
+			$("#play2").prop("disabled", true);
 			var c = 0;
 			interval = setInterval(function() {
 				if (c>12) {
 					clearInterval(interval);	
+					$("#play1").prop("disabled", false);
+					$("#play2").prop("disabled", false);
 				}
 				switch_func(c);
 				c++;
-			}, 3000);
+			}, 500);
 		
 			function switch_func(c) {
 				$("#map").attr("src", "/images/maps/"+map_array[c]);
 			}
 		});		
 		$("#play2").on("click", function() {
+			$("#play1").prop("disabled", true);
+			$("#play2").prop("disabled", true);
 			var c = 0;
 			interval = setInterval(function() {
 				if (c>12) {
 					clearInterval(interval);	
+					$("#play1").prop("disabled", false);
+					$("#play2").prop("disabled", false);
 				}
 				switch_func(c);
 				c++;
-			}, 1000);
+			}, 3000);
 		
 			function switch_func(c) {
 				$("#map").attr("src", "/images/maps/"+map_array[c]);
