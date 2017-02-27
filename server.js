06f960b1e6965a0f28807fbc6c0e000303bf1510
Lindsey Wingate <lindseymwingate@gmail.com>
2017-02-27 18:02:06 +0100

    working on template

--- server.js
+++ server.js
@@ -68,6 +68,16 @@ app.get('/development', function (req, res) {
     });
 });
 
+app.get('/test', function (req, res) {
+	res.render('test', {
+		'title': 'North Dakota Atlas | Test Page',
+		'active': 'Test',
+		'urlbase': dev,
+		'url': url
+	});	
+});
+
+
 ///////////////// Maps /////////////////////////
 app.get('/sugarbeets', function (req, res) {
 	res.render('sugarbeets', {
