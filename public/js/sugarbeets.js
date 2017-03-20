8da9635317c1681c23a0146933ee6c36730de5bb
Lindsey Wingate <lindseymwingate@gmail.com>
2017-03-20 18:43:12 +0100

    adding button to sugarbeets map

--- public/js/sugarbeets.js
+++ public/js/sugarbeets.js
@@ -41,4 +41,26 @@ $(document).ready(function() {
         $("#a_2012").on("click", function() {
                 $("#map").attr("src", "/images/maps/BSugarbeets_2012.png");
         });
+
+        var map_array = ["BSugarbeets_1949-min.png", "BSugarbeets_1954-min.png", "BSugarbeets_1959-min.png", "BSugarbeets_1964-min.png", "BSugarbeets_1969-min.png", "BSugarbeets_1974-min.png", "BSugarbeets_1978-min.png", "BSugarbeets_1982-min.png", "BSugarbeets_1987-min.png", "BSugarbeets_1992-min.png", "BSugarbeets_1997-min.png", "BSugarbeets_2002-min.png", "BSugarbeets_2007-min.png", "BSugarbeets_2012-min.png"];
+        $("#play1").on("click", function() {
+            $("#play1").prop("disabled", true);
+            var c = 0;
+            interval = setInterval(function() {
+                if (c>12) {
+                    clearInterval(interval);    
+                    $("#play1").prop("disabled", false);
+                    $("#play2").prop("disabled", false);
+                }   
+                switch_func(c);
+                c++;
+            }, 2000);
+       
+            function switch_func(c) {
+                $("#map").attr("src", "/images/maps/"+map_array[c]);
+            }   
+        });    
+    }); 
+
+
 });
