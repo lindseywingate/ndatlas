9c8bd7a8be3528456ff993109e0fb1dcd9a4562c
Lindsey Wingate <lindseymwingate@gmail.com>
2017-03-20 21:16:13 +0100

    got corn button working

--- public/js/corn.js
+++ public/js/corn.js
@@ -41,4 +41,21 @@ $(document).ready(function() {
         $("#a_2012").on("click", function() {
                 $("#map").attr("src", "/images/maps/BCorn_2012-min.png");
         });
+var map_array = ["BCorn_1949-min.png", "BCorn_1954-min.png", "BCorn_1959-min.png", "BCorn_1964-min.png", "BCorn_1969-min.png", "BCorn_1974-min.png", "BCorn_1978-min.png", "BCorn_1982-min.png", "BCorn_1987-min.png", "BCorn_1992-min.png", "BCorn_1997-min.png", "BCorn_2002-min.png", "BCorn_2007-min.png", "BCorn_2012-min.png"];
+        $("#play1").on("click", function() {
+            $("#play1").prop("disabled", true);
+            var c = 0;
+            interval = setInterval(function() {
+                if (c>12) {
+                    clearInterval(interval);    
+                    $("#play1").prop("disabled", false);
+                }   
+                switch_func(c);
+                c++;
+            }, 2000);
+       
+            function switch_func(c) {
+                $("#map").attr("src", "/images/maps/"+map_array[c]);
+            }   
+	});
 });
