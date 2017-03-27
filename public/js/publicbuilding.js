0be91aa2f01b59b6670639776e68accb1f9eebcd
Lindsey Wingate <lindseymwingate@gmail.com>
2017-03-27 19:27:58 +0200

    cleaning up css pages.

--- public/js/publicbuilding.js
+++ public/js/publicbuilding.js
@@ -23,21 +23,4 @@ $(document).ready(function() {
         $("#a_1942").on("click", function() {
                 $("#map").attr("src", "/images/maps/WPA_1942-min.png");
         });
-        var map_array = ["WPA_1934-min.png", "WPA_1935-min.png", "WPA_1936-min.png", "WPA_1937-min.png", "WPA_1938-min.png", "WPA_1939-min.png", "WPA_1940-min.png", "WPA_1942-min.png"];
-        $("#play1").on("click", function() {
-            $("#play1").prop("disabled", true);
-            var c = 0;
-            interval = setInterval(function() {
-                if (c>6) {
-                    clearInterval(interval);
-                    $("#play1").prop("disabled", false);
-                }       
-                switch_func(c);
-                c++;
-            }, 2000);
-                
-        function switch_func(c) {
-            $("#map").attr("src", "/images/maps/"+map_array[c]);
-        }       
-    });
 });		
