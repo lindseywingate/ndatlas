9d4598a0074de01f1df0012739a60ad7e285c441
Lindsey Wingate <lindseymwingate@gmail.com>
2017-04-20 16:29:47 +0200

    getting pretty far into paragraphing the hotspots

--- public/js/publicbuilding.js
+++ public/js/publicbuilding.js
@@ -12,16 +12,104 @@ $(document).ready(function() {
 		e.preventDefault();
 		$("#content").html("<h2>Jamestown City Auditorium</h2><p>There is little information available regarding the creation of the Jamestown City Auditorium. Much less is known of it than another auditorium built in the same year mere miles away in McElroy Park, also of Jamestown, North Dakota. The City Auditorium was built by the Works Progress Administration in 1936. Unfortunately, we do not know the architect or the contractor. The building no longer exists, as a 6,500-seat, multi-purpose Civic Center was built in 1973 and the smaller building was no longer needed.</p>");
 	});
-	$("mcelroyaud").on("click", function(e) {
+	$("#mcelroyaud").on("click", function(e) {
 		e.preventDefault();
-		("#content").html("<h2>McElroy Park Auditorium</h2><p>McElroy Park Auditorium in Jamestown, North Dakota, was constructed in 1936, with the assistance of the Works Progress Administration. The auditorium was to be located within the spacious McElroy Park, which is still in use today although the auditorium is long gone. According to an email from Doug Hogan with Jamestown Parks and Recreation, the building was used for some time as an indoor skating rink, but was demolished sometime in the 1960’s. The town referred to the building as a “hippodrome” probably due to its large, curved sides. Since the building is no longer standing, one must rely on written descriptions and one photograph of the exterior. North Dakota: A Guide to the Northern Prairie State was written by another department of the New Deal, The WPA Federal Writer’s Project. The guide describes the building as, “[A] domical building, the design of its façade carried out in the straight lines and angles of modern architecture. Constructed with laminated truss-type arches which support the entire roof load, the auditorium has 25,000 sq. ft. of floor space unobstructed by supporting columns. Its acoustics is excellent owing to the vaulted shape of the roof and the absorbing quality of the timbers in the arches.” According to Hogan, the footprint of the building can still be seen at McElroy Park today. It is currently an outdoor skating rink.</p>");
+		$("#content").html("<h2>McElroy Park Auditorium</h2><p>McElroy Park Auditorium in Jamestown, North Dakota, was constructed in 1936, with the assistance of the Works Progress Administration. The auditorium was to be located within the spacious McElroy Park, which is still in use today although the auditorium is long gone. According to an email from Doug Hogan with Jamestown Parks and Recreation, the building was used for some time as an indoor skating rink, but was demolished sometime in the 1960’s. The town referred to the building as a “hippodrome” probably due to its large, curved sides. Since the building is no longer standing, one must rely on written descriptions and one photograph of the exterior. North Dakota: A Guide to the Northern Prairie State was written by another department of the New Deal, The WPA Federal Writer’s Project. The guide describes the building as, “[A] domical building, the design of its façade carried out in the straight lines and angles of modern architecture. Constructed with laminated truss-type arches which support the entire roof load, the auditorium has 25,000 sq. ft. of floor space unobstructed by supporting columns. Its acoustics is excellent owing to the vaulted shape of the roof and the absorbing quality of the timbers in the arches.” According to Hogan, the footprint of the building can still be seen at McElroy Park today. It is currently an outdoor skating rink.</p>");
 	});
-	$("memhall").on("click", function(e) {
+	$("#memhall").on("click", function(e) {
 		e.preventDefault();
-		("#content").html("<h2>Memorial Hall</h2><p>The small town of Flasher, North Dakota is about half an hour southwest of Bismarck. The main street boasts most of the town’s activity including the Memorial Hall. The Memorial Hall, situated across the street from the Flasher’s post office is one among several Memorial Halls build in the state with WPA assistance. The hall was completed in 1936, and it features a plaque dedicating the building to fallen World War I and World War II soldiers. Unfortunately, like so many of the smaller WPA projects, it is difficult to locate much information about the construction of the building.</p>");
-});
+		$("#content").html("<h2>Memorial Hall</h2><p>The small town of Flasher, North Dakota is about half an hour southwest of Bismarck. The main street boasts most of the town’s activity including the Memorial Hall. The Memorial Hall, situated across the street from the Flasher’s post office is one among several Memorial Halls build in the state with WPA assistance. The hall was completed in 1936, and it features a plaque dedicating the building to fallen World War I and World War II soldiers. Unfortunately, like so many of the smaller WPA projects, it is difficult to locate much information about the construction of the building.</p>");
+	});
 
+	$("#pemhall").on("click", function(e) {
+		e.preventDefault();
+		$("#content").html("<h2>City Hall</h2><p>Located about seventy miles north of Grand Forks, lies Pembina, North Dakota, on the border of North Dakota and Manitoba, Canada. The Pembina City Hall was one of several Federal Relief projects in the town. It was completed in 1936 by the PWA to serve as the city offices. Now it sits next to the current city offices and serves as a community center. There is not a lot to distinguish its status as a Federal Relief project building due to the renovations made to the building.</p>");
+	});
 
+	$("#newmemhall").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>Memorial Hall</h2><p>New England, North Dakota is located approximately thirty miles south of Dickinson, North Dakota in the southwest quadrant of the state, in Hettinger County. The Memorial Hall was built in 1936 with the assistance of the Public Works Administration. The building currently acts as a multi-purpose facility, and houses the local chapters of the American Legion, Boy Scout and Girl Scout troops. The Memorial Hall was built in the Art Moderne style, as many Federal Relief projects were. According to Steven Martens’ Multiple Place Form for Federal Relief construction in North Dakota, the architectural firm of VanHorn and Ritterbush from Bismarck were responsible for the design of the project. The building has seen a few renovations since its creation. In February 2016, The Dickinson Press, released a story about the current renovations taking place at the Memorial Hall. According to the article, the building has tried to retain all of the original character of the 1936 construction. Upon viewing the Memorial Hall, one can tell that it is a treasured landmark in the New England community.</p>");
+	});
+
+	$("#centralhigh").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>Central High School Auditorium</h2><p>The auditorium in Central High School in downtown Grand Forks, replaced the original structure in 1937. It was built with the assistance of the WPA, however, since it was an addition there is not much information available about the construction. It was constructed by Samuel T. DeRemer. It does appear to have been recently renovated. The structure is impossible to miss, as it is a rather large building with a unique structure for the area. The Federal Writer’s Project describes the unit as a windowless auditorium and bestows upon it the distinction of being the first building in North Dakota to use indirect light throughout. The cost was $275,000 and included a pipe organ.</p>");
+	});
+
+	$("#renville").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>Renville County Courthouse</h2><p>The north-central town of Mohall, North Dakota is the county seat of Renville County, and as such, the location for the county courthouse. It has served consistently as the storehouse for county records and the center of county government since its construction in 1937. The building also houses a jail, which has been in use since the construction. It was built through the assistance of the Works Progress Administration and was the largest county expenditure during the 1930’s. Out of the nine courthouses constructed in North Dakota from 1929-1940, the Renville County Courthouse is the only one that displays an Art Moderne styling, as opposed to Art Deco. It is characterized by its rounded corners, a low, horizontal profile, and circle-window doors in the courtroom. The façade is mostly brick, except for a parapet that is covered in Kettle River sandstone brought to the site from Minnesota. The name “Memorial Court House” is still present on the frieze. The interior features bronze doors, walls of Montana travertine marble, and terrazzo floors. The courthouse is one of the more ornate of the Federal Relief buildings in North Dakota.</p>");
+	});
+
+	$("#stark").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>Stark County Courthouse</h2><p>Found in the western North Dakota town of Dickinson, the Stark County Courthouse still serves as the center of county government. Completed in 1937, the building’s construction budget accounted for over half of what was considered a construction boom in the county. The cost of the building was $213,000 and was helped in part by the Public Works Administration. The town had to undertake a $100,000 bonded indebtedness, but the vote passed with little resistance because the Stark County residents were encouraged by the progress. The building boom in the county was a good sign that they had made it through the worst part of the Depression. The bond was worth it to provide the economic and employment relief that was plaguing the area during the Great Depression.</p>");
+	});
+
+	$("#feds").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>Federal Extension Building</h2><p>Historically, the Federal Building in downtown Bismarck was used as a post office and courthouse. The building was first completed in 1913, but was extended in 1937 using Treasury Department funds, as part of the Federal Emergency Relief Act. The extension was built using the same materials, however, if one looks at the building it is easy to see where the extension begins. The original expansion was made to enlarge the size of the postal workroom. The interior of the building little resembles the original design of the building as it has changed uses several times over the course of time.</p>");
+	});
+	$("#westhope").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>Inspection Station</h2><p>Many border inspection stations were created through the Federal Emergency Relief Act with Treasury Department funds in the 1930s. Most stations, like Westhope and Sherwood, were built on new highways coming in and out of the country. Westhope Inspection Station was built in 1937. It was a wood-framed, single story garage, which still exists today, however it has been heavily altered and retains none of the original integrity of the building.</p>");
+	});
+	$("#langdonpost").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>Langdon Post Office</h2><p>Langdon, North Dakota is nearly two hours north of Grand Forks, close to the Canadian border. The post office was constructed in 1937, with funds from the US Treasury Department. The amount of $70,000 was appropriated to build the post office and it provided jobs for twenty men from Langdon. The building, like most of the other post offices covered in this project, is in the “starved classical” style. It is currently the oldest public building in Langdon, serves as the only federal building, and is kept in very good repair. The building also was the recipient of a WPA-commissioned artwork by Leo J. Beaulaurier in 1939, entitled “Indians Demanding Toll from Wagon Train for Crossing Their Land.” Beaulaurier had mistaken the Red River cart trails to resemble those of the Santa Fe Trail in the south, so the mural is out of place, but interesting nonetheless.</p>");
+	});
+	$("#tuttle").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>City Hall</h2><p>Tuttle, North Dakota is found approximately an hour and a half northeast of Bismarck. The City Hall was built by the WPA in 1938 and served for some time as the city post office in addition its duties as City Hall. It was one of the smaller construction projects in North Dakota, and the building itself is simple. According to Lorna Meidinger, architectural historian with the State Historical Society, the building was gutted by a fire in November of 2007 when a furnace was ignited. The future of the building’s fieldstone wall was in jeopardy for a time, but it seems as though an unknown individual or group has taken control of the property and has at least made the building stable. It is not certain what will be done with the building as it is.</p>");
+	});
+	$("#firehall").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>Fire Hall</h2><p>Tappen, North Dakota, lies a half hour to the west of Bismarck. After suffering a fire that destroyed downtown Tappen in 1926, a rebuilding effort took place in the late 1920s through the 1930s. The WPA stepped in to help fund a new fire hall, located in the very center of the town. The small, unimposing, stone building was completed in 1938. Today, it is no longer used as the town’s fire hall, although the name and date of construction are displayed prominently on the structure.</p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("# ").on("click", function(e) {
+		e.preventDefault();	
+		$("# ").html("<h2> </h2><p> </p>");
+	});
+	$("#sherwood").on("click", function(e) {
+		e.preventDefault();	
+		$("#content").html("<h2>Inspection Station</h2><p>The New Deal enabled the building of many border inspection stations across the country through Treasury Department funds. Interestingly enough, they all were thematic based on where they were located. The Sherwood Inspection Station was built in 1937 and was deemed necessary because of the opening of a highway going north into Canada. The building was demolished and replaced with a modern border crossing facility in 1982. Documentation is not explicitly clear, but it indicates that a storage building that was built at the same time of the station house may still be on the original site today.</p>");
+	});
 
 	//button js for switching maps	
 	$("#a_1934").on("click", function() {
