511be8c1d4eb1e667941c1b812ee9c114a3c392c
Lindsey Wingate <lindseymwingate@gmail.com>
2017-04-23 18:13:59 +0200

    got menus centering again

--- public/js/missiles.js
+++ public/js/missiles.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 		$("facilities").on("click", function() {
-                $("#map").attr("src", "/images/maps/Missile_Facilities.png");
-        });
-        $("#range").on("click", function() {
-                $("#map").attr("src", "/images/maps/Missile_Range.png");
-        });
+			$("#map").attr("src", "/images/maps/Missile_Facilities.png");
+		});
+		$("#range").on("click", function() {
+			$("#map").attr("src", "/images/maps/Missile_Range.png");
+		});
+	});
