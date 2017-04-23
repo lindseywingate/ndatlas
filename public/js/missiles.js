92889b16f996720b87d84adefce8bbf3c7d15ae1
Lindsey Wingate <lindseymwingate@gmail.com>
2017-04-23 17:41:37 +0200

    new pics

---
+++ public/js/missiles.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+		$("facilities").on("click", function() {
+                $("#map").attr("src", "/images/maps/Missile_Facilities.png");
+        });
+        $("#range").on("click", function() {
+                $("#map").attr("src", "/images/maps/Missile_Range.png");
+        });
