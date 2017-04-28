9e710deb9024aa15db49e9feff3f9653cae57799
Lindsey Wingate <lindseymwingate@gmail.com>
2017-04-28 14:51:24 +0200

    stupid id was missing #

--- public/js/missiles.js
+++ public/js/missiles.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-		$("facilities").on("click", function() {
+		$("#facilities").on("click", function() {
 			$("#map").attr("src", "/images/maps/Missile_Facilities.png");
 		});
 		$("#range").on("click", function() {
